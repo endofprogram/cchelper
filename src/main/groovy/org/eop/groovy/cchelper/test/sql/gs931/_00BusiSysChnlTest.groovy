41fed4aae682f431d34cec47552d9528c5fff485
wangmaomao <hisoftworm@hotmail.com>
2017-11-16 22:33:38 +0800

    update

--- src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/_00BusiSysChnlTest.groovy
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/_00BusiSysChnlTest.groovy
@@ -13,7 +13,7 @@ class _00BusiSysChnlTest extends AbstractSqlTest {
 
 	@Override
 	Map getSqlFile() {
-		[dir:'E:\\luying\\甘肃在线公司\\需求\\众包接口\\数据库\\测试环境\\', name:'00系统渠道']
+		[dir:'E:\\Work\\甘肃在线公司\\甘肃在线公司\\需求\\互联网15个接口\\数据库\\测试环境\\', name:'00系统渠道']
 	}
 	
 	@Override
@@ -36,6 +36,10 @@ class _00BusiSysChnlTest extends AbstractSqlTest {
 		def list = []
 		list << [type:'GS_NL_PLTFM', fst:'zhongbao20170922', sec:'appId', val:'A2017083117061000024', rmk:'']
 		list << [type:'GS_NL_PLTFM', fst:'zhongbao20170922', sec:'accessToken', val:'4920cb16c8bdf0442c3aca5b5a7123b37fb10b5a', rmk:'']
+		list << [type:'GS_NL_PLTFM', fst:'intnet1520171011', sec:'appId', val:'A2017081710261500016', rmk:'']
+		list << [type:'GS_NL_PLTFM', fst:'intnet1520171011', sec:'accessToken', val:'84e3b4f3b25bac4442e497706338d3b8230f8a97', rmk:'']
+		list << [type:'GS_NL_PLTFM', fst:'ivrznyydh20171012', sec:'appId', val:'A2017110316010500006', rmk:'']
+		list << [type:'GS_NL_PLTFM', fst:'ivrznyydh20171012', sec:'accessToken', val:'8f0f2042aad43c84459f7d5c90bedb2110c7fff8', rmk:'']
 		list << [type:'GS_NL_PLTFM', fst:'nengkai20170922', sec:'tokenInInterfaceId', val:'', rmk:'']
 		list << [type:'GS_NL_PLTFM', fst:'nengkai20170922', sec:'tokenOutInterfaceId', val:'', rmk:'']
 		list
