8afe24a53f3dab60420e330de19d8f72e9e1a146
wangmaomao <hisoftworm@hotmail.com>
2017-12-07 19:52:06 +0800

    add one more dept

---
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/i03_03MoBizQry.groovy
@@ -0,0 +1,34 @@
+package org.eop.groovy.cchelper.test.sql.gs931
+
+class i03_03MoBizQry extends _00Int15CommonOneSqlTest {
+
+    @Override
+    String getSqlFilePrefix() {
+        'i03_03'
+    }
+
+    @Override
+    String getDistinctName() {
+        '查询在用梦网业务'
+    }
+
+    @Override
+    String getDepKey() {
+        'usermobiz'
+    }
+
+    @Override
+    Map getInIntfMap() {
+        ['path': 'business/gs/userMoBizQry', 'method': 'GET']
+    }
+
+    @Override
+    List getInParams() {
+        ['userMobile']
+    }
+
+    @Override
+    Map getOutIntfMap() {
+        ['cls': 'gsUserCurMoBizQueryService', 'path': '/openapi/V1/partner/ability/production/queryAllRadiusInfo', 'method': 'POST']
+    }
+}
