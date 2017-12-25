febc123a0575dc71766a5935ea80ca6cb5115452
znn <1576011157@qq.com>
2017-12-25 17:03:28 +0800

    ivr初始化

---
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/i14ProdOrderBusi.groovy
@@ -0,0 +1,36 @@
+package org.eop.groovy.cchelper.test.sql.gs931
+
+class i14ProdOrderBusi extends _00Int15CommonUnionSqlTest{
+
+    @Override
+    String getSqlFilePrefix() {
+        'i14'
+    }
+
+    @Override
+    String getDistinctName() {
+        '业务办理'
+    }
+
+    @Override
+    List getDeps() {
+        ['业务办理产品受理', '业务办理省数据业务受理']
+    }
+
+    @Override
+    Map getInIntfMap() {
+        ['path': 'business/gs/prodOrderBusiTrans', 'method': 'post']
+    }
+
+    @Override
+    List getInParams() {
+        ['userMobile','oprCode','productId','prodType','effectiveType','busiType','spId','servCode']
+    }
+
+    @Override
+    Map getOutIntfMap() {
+        ['cls': 'gsProdOrderService']
+    }
+
+
+}
