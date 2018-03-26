7fd2c38fb5d823c4de7e66bae20aea7253a93169
mafuliang <337668909@qq.com>
2018-03-26 14:44:52 +0800

    查询缴费记录

---
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/broadband/GsQueryPayRecordsQueryService.groovy
@@ -0,0 +1,34 @@
+package org.eop.groovy.cchelper.test.sql.gs931.broadband
+
+class GsQueryPayRecordsQueryService extends BroadbandByNengkaiCommonOneSqlTest {
+
+    @Override
+    String getSqlFilePrefix() {
+        'payRecords'
+    }
+
+    @Override
+    String getDistinctName() {
+        '缴费记录查询'
+    }
+
+    @Override
+    String getDepKey() {
+        'QueryPayRecords'
+    }
+    @Override
+    Map getInIntfMap() {
+        ['cls':'gsQueryPayRecordsQueryInService','path': 'business/gs/QueryPayRecords', 'method': 'GET']
+    }
+
+    @Override
+    List getInParams() {
+        ['busiCode','startTime','endTime']
+    }
+
+    @Override
+    Map getOutIntfMap() {
+        [ 'cls':'gsQueryPayRecordsQueryOutService', 'path': '/openapi/V1/Charge/Book/queryBroadbandPaymentRecords4Online', 'method': 'POST']
+    }
+}
+
