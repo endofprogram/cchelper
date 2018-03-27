8980578a21ff96482d2da2899c077cd60463f654
上海陆鹰-崔贝贝 <932601534@qq.com>
2018-03-27 17:43:09 +0800

    家宽接口sql配置

---
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/broadband/GsBroadbandArrearageInfoQueryService.groovy
@@ -0,0 +1,36 @@
+package org.eop.groovy.cchelper.test.sql.gs931.broadband
+
+import java.util.List
+import java.util.Map
+
+class GsBroadbandArrearageInfoQueryService extends BroadbandByNengkaiCommonOneSqlTest{
+
+	@Override
+	String getSqlFilePrefix() {
+		'broadband'
+	}
+
+	@Override
+	String getDistinctName() {
+		'查询宽带帐号状态-是否欠费接口'
+	}
+
+	@Override
+	String getDepKey() {
+		'BroadbandArrearageInfo'
+	}
+	@Override
+	Map getInIntfMap() {
+		['cls':'gsBroadbandArrearageInfoQueryInService','path': 'business/gs/BroadbandArrearageInfo', 'method': 'GET']
+	}
+
+	@Override
+	List getInParams() {
+		['busiCode']
+	}
+
+	@Override
+	Map getOutIntfMap() {
+		[ 'cls':'gsBroadbandArrearageInfoQueryOutService', 'path': '/openapi/V1/Charge/Allowance/queryBroadbandOweFee4Online', 'method': 'POST']
+	}
+}
