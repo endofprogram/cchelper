7ed65f035b310c9516bc3b4681588e119cbbd5f3
mafuliang <337668909@qq.com>
2018-04-19 10:13:53 +0800

    查询客户资料信息

--- src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/online/GssyncActivateInforQueryService.groovy
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/online/GssyncActivateInforQueryService.groovy
@@ -2,31 +2,31 @@ package org.eop.groovy.cchelper.test.sql.gs931.online
 
 class GssyncActivateInforQueryService extends OnlineByNengkaiCommonOneSqlTest {
     @Override
-    public String getSqlFilePrefix() {
+     String getSqlFilePrefix() {
         'online_01'
     }
 
     @Override
-    public String getDistinctName() {
+     String getDistinctName() {
         '认证成功手机卡激活'
     }
 
     @Override
-    public String getDepKey() {
+     String getDepKey() {
         'ObtainOfferFee'
     }
 
     @Override
-    public Map getInIntfMap() {
+     Map getInIntfMap() {
         ['cls':'gssyncActivateInforQueryInService','path': 'business/gs/ObtainOfferFee', 'method': 'POST']
     }
     @Override
-    public List getInParams() {
+    List getInParams() {
         ['reqInfo']
     }
 
     @Override
-    public Map getOutIntfMap() {
+    Map getOutIntfMap() {
         [ 'cls':'gssyncActivateInforQueryOutService', 'path': '/openapi/V1/Party/User/syncActivateInformation4Online', 'method': 'POST']
     }
 }
--- src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/online/GssyncOrderInforQueryService.groovy
+++ src/main/groovy/org/eop/groovy/cchelper/test/sql/gs931/online/GssyncOrderInforQueryService.groovy
@@ -2,31 +2,33 @@ package org.eop.groovy.cchelper.test.sql.gs931.online
 
 class GssyncOrderInforQueryService  extends OnlineByNengkaiCommonOneSqlTest {
     @Override
-    public String getSqlFilePrefix() {
+  String getSqlFilePrefix() {
         'online_05'
     }
 
     @Override
-    public String getDistinctName() {
+
+
+    String getDistinctName() {
         '预售卡'
     }
 
     @Override
-    public String getDepKey() {
+    String getDepKey() {
         'PresaleCardOrder'
     }
 
     @Override
-    public Map getInIntfMap() {
+    Map getInIntfMap() {
         ['cls':'gssyncOrderInforQueryInService','path': 'business/gs/PresaleCardOrder', 'method': 'POST']
     }
     @Override
-    public List getInParams() {
+   List getInParams() {
         ['reqInfo']
     }
 
     @Override
-    public Map getOutIntfMap() {
+    Map getOutIntfMap() {
         [ 'cls':'gssyncOrderInforQueryOutService', 'path': '/openapi/V1/BizInteraction/Order/syncOrderInformation4Online', 'method': 'POST']
     }
 }
